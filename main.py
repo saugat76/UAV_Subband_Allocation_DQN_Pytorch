7e89df88a0b2a6e98100019e85af26577066fa7f
Tripathi <tripats@miamioh.edu>
2022-07-18 03:01:18 -0400

    Code DQL for comparision with QL - Final edited working

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from ctypes.wintypes import tagRECT
 from os import stat_result, times_result
 import random
 from re import S
+import re
 from sre_parse import State
 from ssl import ALERT_DESCRIPTION_USER_CANCELLED
 from tabnanny import verbose
@@ -28,7 +29,8 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 PATH_DATASETS = os.environ.get("PATH_DATASETS", ".")
 
 ## GPU configuration use for faster processing
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+device = "cpu"
 
 # DNN modeling
 class NeuralNetwork(nn.Module):
@@ -36,13 +38,20 @@ class NeuralNetwork(nn.Module):
         super(NeuralNetwork, self).__init__()
         self.state_size = state_size
         self.action_size = action_size
-        self.linear_stack = nn.Sequential(
-            nn.Linear(self.state_size,128),
+        self.linear_stack = model = nn.Sequential(
+            nn.Linear(self.state_size,256),
+            nn.ReLU(),
+            nn.Linear(256,128),
+            nn.ReLU(),
+            nn.Linear(128,128),
+            nn.ReLU(),
             nn.Linear(128,64),
+            nn.ReLU(),
             nn.Linear(64, self.action_size)
-        )
+        ).to(device=device)
 
     def forward(self, x):
+        x = x.to(device)
         Q_values = self.linear_stack(x)
         return Q_values
 
@@ -55,10 +64,9 @@ class DQL:
         self.replay_buffer = deque(maxlen = 125000)
         self.gamma = 0.90
         self.epsilon = 0.1
-        self.update_rate = 20
-        self.learning_rate = 0.025
-        self.main_network = NeuralNetwork(self.state_size, self.action_size)
-        self.target_network = NeuralNetwork(self.state_size, self.action_size)
+        self.learning_rate = 0.0075
+        self.main_network = NeuralNetwork(self.state_size, self.action_size).to(device)
+        self.target_network = NeuralNetwork(self.state_size, self.action_size).to(device)
         self.target_network.load_state_dict(self.main_network.state_dict())
         self.optimizer = torch.optim.Adam(self.main_network.parameters(), lr = self.learning_rate)
         self.loss_func = nn.MSELoss()
@@ -69,64 +77,65 @@ class DQL:
     
     # Deployment of epsilon greedy policy
     def epsilon_greedy(self, state):
-        random.seed(10)   
         temp = random.random()
         if temp <= self.epsilon:
-            action = random.randint(0, 4)
+            action = np.random.randint(0, 4)
         else:
             state = torch.unsqueeze(torch.FloatTensor(state),0)
+            # print(state)
             Q_values = self.main_network.forward(state)
             # print(Q_values)
-            action = torch.max(Q_values, 1)[1].data.numpy()
+            action = torch.max(Q_values.cpu(), 1)[1].data.numpy()
             # print(action)
             action = action[0]
         return action
 
     # Training of the DNN 
-    def train(self,batch_size_internal, dnn_epoch):
-    
+    def train(self,batch_size, dnn_epoch, batch_size_internal):
+        # internal_buffer = random.sample(self.replay_buffer, batch_size)
+        for k in range(dnn_epoch):
             minibatch = random.sample(self.replay_buffer, batch_size_internal)
             minibatch = np.vstack(minibatch)
-            minibatch = minibatch.reshape(batch_size,5)
+            minibatch = minibatch.reshape(batch_size_internal,5)
             state = torch.FloatTensor(np.vstack(minibatch[:,0]))
             # print(state)
             action = torch.LongTensor(np.vstack(minibatch[:,1]))
             reward = torch.FloatTensor(np.vstack(minibatch[:,2]))
             next_state = torch.FloatTensor(np.vstack(minibatch[:,3]))
             done = torch.Tensor(np.vstack(minibatch[:,4]))
-                    
-            Q_main = self.main_network(state).gather(1, action).squeeze()
+            state = state.to(device = device)
+            action = action.to(device = device)
+            reward = reward.to(device = device)
+            next_state = next_state.to(device = device)
+            done = done.to(device = device)
+
             Q_next = self.target_network(next_state).detach()
-            target_Q = reward.squeeze() + self.gamma * Q_next.max(1)[0].view(batch_size_internal, 1).squeeze() * (
-                1 - np.array([state[e].mean() == next_state[e].mean() for e in range(len(next_state))])
+            target_Q = reward.cpu().squeeze() + self.gamma * Q_next.cpu().max(1)[0].view(batch_size_internal, 1).squeeze() * (
+                1 - np.array([state[e].cpu().mean() == next_state[e].cpu().mean() for e in range(len(next_state))])
             ) 
-
             target_Q = target_Q.float()
+            Q_main = self.main_network(state).gather(1, action).squeeze()
 
-            loss = self.loss_func(Q_main, target_Q.detach())
+            loss = self.loss_func(Q_main.cpu(), target_Q.cpu().detach())
 
             self.optimizer.zero_grad()
             loss.backward()
             self.optimizer.step()
 
 
-    # # Updating the weights of the target network from the main network
-    # def update_target_network(self):
-    #     self.target_network.set_weights(self.main_network.get_weights())
-
-
 u_env = UAVenv()
 GRID_SIZE = u_env.GRID_SIZE
 NUM_UAV = u_env.NUM_UAV
 NUM_USER = u_env.NUM_USER
-num_episode = 200
-num_epochs = 500
-dnn_epoch = 100
+num_episode = 401
+num_epochs = 100
 discount_factor = 0.90
 alpha = 0.5
-epsilon = 0.1
-batch_size = 2048
-update_rate = 20
+batch_size = 512
+batch_size_internal = 512
+update_rate = 10  #50
+dnn_epoch = 1
+train_freq = 32 #NA
 
 random.seed(10)
 
@@ -163,15 +172,19 @@ for i_episode in range(num_episode):
                 UAV_OB[k].target_network.load_state_dict(UAV_OB[k].main_network.state_dict())
                 
         # Determining the actions for all drones
+        # print(states)
         states_ten = torch.from_numpy(states)
+        # print(states_ten)
         for k in range(NUM_UAV):
-            state = states_ten[k,:]
+            state = states_ten[k, :]
             # print(state)                
             action = UAV_OB[k].epsilon_greedy(state.float())
             # print(action)
             drone_act_list.append(action + 1)
             # print('new')
-    
+
+        # print(drone_act_list)
+
         # Find the global reward for the combined set of actions for the UAV
         # print(drone_act_list)
         temp_data = u_env.step(drone_act_list)
@@ -181,7 +194,7 @@ for i_episode in range(num_episode):
 
         # Store the transition information
         for k in range(NUM_UAV):
-                state = states[k, :]
+                state = states_ten[k, :]
                 action = drone_act_list[k] - 1
                 next_sta = next_state[k, :]
                 UAV_OB[k].store_transition(state, action, reward, next_sta, done)
@@ -190,10 +203,12 @@ for i_episode in range(num_episode):
 
         states = next_state
 
+        # if done:
+        #     break
+
         for k in range(NUM_UAV):
             if len(UAV_OB[k].replay_buffer) > batch_size:
-                # with strategy.scope():
-                UAV_OB[k].train(batch_size, dnn_epoch)
+                UAV_OB[k].train(batch_size, dnn_epoch, batch_size_internal)
 
     if i_episode % 10 == 0:
         # Reset of the environment
@@ -209,16 +224,16 @@ for i_episode in range(num_episode):
                 state = torch.unsqueeze(torch.FloatTensor(state),0)
                 Q_values = UAV_OB[k].main_network.forward(state.float())
                 # print(Q_values)
-                best_next_action = torch.max(Q_values, 1)[1].data.numpy()
+                best_next_action = torch.max(Q_values.cpu(), 1)[1].data.numpy()
                 best_next_action = best_next_action[0]
                 drone_act_list.append(best_next_action + 1)
-            
             temp_data = u_env.step(drone_act_list)
             states = u_env.get_state()
             states_fin = states
+        u_env.render(ax1)
         print(drone_act_list)
         print("Number of user connected in ",i_episode," episode is: ", temp_data[4])
-        u_env.render(ax1)
+
 
 
 def smooth(y, pts):
@@ -239,4 +254,4 @@ final_render(states_fin)
 # mdict = {'Q': Q_values}
 # savemat('Q.mat', mdict)
 print(states_fin)
-print('Total Connected User in Final Stage', reward)
+print('Total Connected User in Final Stage', temp_data[4])
