f11861c8c12ac1aa4c5e697e17057c0cec3e1b0b
Saugat Tripathi <saugat.tripathi76@gmail.com>
2022-11-08 21:44:12 -0500

    Changed hyperparameters

--- uav_env.py
+++ uav_env.py
@@ -236,15 +236,15 @@ class UAVenv(gym.Env):
         ################################################################
         ##     Opt.1  No. of User Connected as Indiviudal Reward      ##
         ################################################################
-        sum_user_assoc = np.sum(user_asso_flag, axis = 1)
-        reward_solo = np.zeros(np.size(sum_user_assoc), dtype="float32")
-        for k in range(self.NUM_UAV):
-            if self.flag[k] != 0:
-                reward_solo[k] = np.copy(sum_user_assoc[k] - 2)
-                isDone = True
-            else:
-                reward_solo[k] = np.copy(sum_user_assoc[k]) 
-        reward = np.copy(reward_solo)
+        # sum_user_assoc = np.sum(user_asso_flag, axis = 1)
+        # reward_solo = np.zeros(np.size(sum_user_assoc), dtype="float32")
+        # for k in range(self.NUM_UAV):
+        #     if self.flag[k] != 0:
+        #         reward_solo[k] = np.copy(sum_user_assoc[k] - 2)
+        #         isDone = True
+        #     else:
+        #         reward_solo[k] = np.copy(sum_user_assoc[k]) 
+        # reward = np.copy(reward_solo)
 
         #############################################################################################
         ##     Opt.2  No. of User Connected as Indiviudal Reward with Penalty Over Buffer Area     ##
@@ -265,20 +265,20 @@ class UAVenv(gym.Env):
         ################################################################
         ##     Opt.3  No. of User Connected as Collective Reward      ##
         ################################################################
-        # sum_user_assoc = np.sum(user_asso_flag, axis = 1)
-        # sum_user_assoc_temp = np.copy(sum_user_assoc)
-        # reward_ind = np.zeros(np.size(sum_user_assoc))
-        # reward = 0
-        # for k in range(self.NUM_UAV):
-        #     if self.flag[k] != 0:
-        #         sum_user_assoc_temp[k] -= 2
-        #         temp_user_id = np.where(dist_uav_uav[k, :] <= self.UAV_DIST_THRS)
-        #         reward_ind[k] = np.average(sum_user_assoc_temp[temp_user_id])
-        #         isDone = True
-        #     else:
-        #         temp_user_id = np.where(dist_uav_uav[k, :] <= self.UAV_DIST_THRS)
-        #         reward_ind[k] = np.average(sum_user_assoc[temp_user_id])
-        # reward = np.copy(reward_ind)
+        sum_user_assoc = np.sum(user_asso_flag, axis = 1)
+        sum_user_assoc_temp = np.copy(sum_user_assoc)
+        reward_ind = np.zeros(np.size(sum_user_assoc))
+        reward = 0
+        for k in range(self.NUM_UAV):
+            if self.flag[k] != 0:
+                sum_user_assoc_temp[k] -= 2
+                temp_user_id = np.where(dist_uav_uav[k, :] <= self.UAV_DIST_THRS)
+                reward_ind[k] = np.average(sum_user_assoc_temp[temp_user_id])
+                isDone = True
+            else:
+                temp_user_id = np.where(dist_uav_uav[k, :] <= self.UAV_DIST_THRS)
+                reward_ind[k] = np.average(sum_user_assoc[temp_user_id])
+        reward = np.copy(reward_ind)
 
         
         # Defining the reward function by the number of covered user
