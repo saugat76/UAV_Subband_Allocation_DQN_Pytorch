1e18c0732929061e98e55ffdf4664c4076975701
Saugat Tripathi <saugat.tripathi76@gmail.com>
2023-02-28 11:18:26 -0500

    added few details

--- resultplot.py
+++ resultplot.py
@@ -23,42 +23,42 @@ matplotlib.rc('font', **font)
 # Aplha / Learning Rate = 3.5e-4
 
 ## Plot for data from NUM_UAV - 5
-episode_reward_level_1 = loadmat(r'Results\Result_11_08\5_UAV\Level_1_Implicit_Info_Exchange\Run_002\episodic_reward.mat')
-episode_reward_level_2 = loadmat(r'Results\Result_11_08\5_UAV\Level_2_Reward_Info_Exchange\Run_002\episodic_reward.mat')
-episode_reward_level_3 = loadmat(r'Results\Result_11_08\5_UAV\Level_3_Position_of_UAV_(Distance_Penalty)\Run_002\episodic_reward.mat')
-episode_reward_level_4 = loadmat(r'Results\Result_11_08\5_UAV\Level_4_Drone_State_Space_Exchange\Run_002\episodic_reward.mat')
+# episode_reward_level_1 = loadmat(r'Results\Result_11_08\5_UAV\Level_1_Implicit_Info_Exchange\Run_002\episodic_reward.mat')
+# episode_reward_level_2 = loadmat(r'Results\Result_11_08\5_UAV\Level_2_Reward_Info_Exchange\Run_002\episodic_reward.mat')
+# episode_reward_level_3 = loadmat(r'Results\Result_11_08\5_UAV\Level_3_Position_of_UAV_(Distance_Penalty)\Run_002\episodic_reward.mat')
+# episode_reward_level_4 = loadmat(r'Results\Result_11_08\5_UAV\Level_4_Drone_State_Space_Exchange\Run_002\episodic_reward.mat')
 
 
-num_episode_1 = list(episode_reward_level_1['num_episode'])
-episode_reward_1 = list(episode_reward_level_1['episodic_reward'])
-num_episode_2 = list(episode_reward_level_2['num_episode'])
-episode_reward_2 = list(episode_reward_level_2['episodic_reward'])
-num_episode_3 = list(episode_reward_level_3['num_episode'])
-episode_reward_3 = list(episode_reward_level_3['episodic_reward'])
-num_episode_4 = list(episode_reward_level_4['num_episode'])
-episode_reward_4 = list(episode_reward_level_4['episodic_reward'])
+# num_episode_1 = list(episode_reward_level_1['num_episode'])
+# episode_reward_1 = list(episode_reward_level_1['episodic_reward'])
+# num_episode_2 = list(episode_reward_level_2['num_episode'])
+# episode_reward_2 = list(episode_reward_level_2['episodic_reward'])
+# num_episode_3 = list(episode_reward_level_3['num_episode'])
+# episode_reward_3 = list(episode_reward_level_3['episodic_reward'])
+# num_episode_4 = list(episode_reward_level_4['num_episode'])
+# episode_reward_4 = list(episode_reward_level_4['episodic_reward'])
 
 
-num_episode_1 = num_episode_1[0]
-episode_reward_1 = episode_reward_1[0]
-num_episode_2 = num_episode_2[0]
-episode_reward_2 = episode_reward_2[0]
-num_episode_3 = num_episode_3[0]
-episode_reward_3 = episode_reward_3[0]
-num_episode_4 = num_episode_4[0]
-episode_reward_4 = episode_reward_4[0]
+# num_episode_1 = num_episode_1[0]
+# episode_reward_1 = episode_reward_1[0]
+# num_episode_2 = num_episode_2[0]
+# episode_reward_2 = episode_reward_2[0]
+# num_episode_3 = num_episode_3[0]
+# episode_reward_3 = episode_reward_3[0]
+# num_episode_4 = num_episode_4[0]
+# episode_reward_4 = episode_reward_4[0]
 
 
-fig = plt.figure()
-plt.plot(num_episode_1[0:350], episode_reward_1[0:350], 'r', label='Level 1: Implicit')
-plt.plot(num_episode_2[0:350], episode_reward_2[0:350], 'g', label='Level 2: Reward exchange')
-plt.plot(num_episode_3, episode_reward_3, 'b', label='Level 3: Distance penalty')
-plt.plot(num_episode_4, episode_reward_4, 'm', label='Level 4: State')
-plt.legend(loc="lower right")
-plt.xlabel("Episode", fontsize = 14, family = 'Times New Roman')
-plt.ylabel("Episodic reward", fontsize = 14, family = 'Times New Roman')
-plt.title("Episode vs episodic reward with run002 parameters")
-plt.show()
+# fig = plt.figure()
+# plt.plot(num_episode_1[0:350], episode_reward_1[0:350], 'r', label='Level 1: Implicit')
+# plt.plot(num_episode_2[0:350], episode_reward_2[0:350], 'g', label='Level 2: Reward exchange')
+# plt.plot(num_episode_3, episode_reward_3, 'b', label='Level 3: Distance penalty')
+# plt.plot(num_episode_4, episode_reward_4, 'm', label='Level 4: State')
+# plt.legend(loc="lower right")
+# plt.xlabel("Episode", fontsize = 14, family = 'Times New Roman')
+# plt.ylabel("Episodic reward", fontsize = 14, family = 'Times New Roman')
+# plt.title("Episode vs episodic reward with run002 parameters")
+# plt.show()
 
 # ## Plot for data from NUM_UAV - 7 and NUM_UAV - 5 Compari
 # episode_reward_7_level_4 = loadmat(r'Results\Result_11_08\7_UAV\Level_4_Drone_State_Space_Exchange\Run_002\episodic_reward.mat')
@@ -199,3 +199,31 @@ plt.show()
 # plt.ylabel("Episodic Reward")
 # plt.title("Episode vs Episodic Reward for UAV 7 : Level 3")
 # plt.show()
+
+# ###############################################################
+# ####  Level 3 Different Distance Threshold Values          ####
+# ###############################################################
+episode_reward_16 = loadmat(r'C:\Users\tripats\Documents\Results_SameParams0017\5_UAV\Distance Threshold Neighbours\Distance_Threshold300\Level_3_Position_of_UAV_(Distance_Penalty)\Starting_Pos(0,0)\episodic_reward.mat')
+episode_reward_14 = loadmat(r'C:\Users\tripats\Documents\Results_SameParams0017\5_UAV\Distance Threshold Neighbours\Distance_Threshold1000\Level_3_Position_of_UAV_(Distance_Penalty)\Starting_Pos(0,0)\Distance_penalty_priority_1over4\episodic_reward.mat')
+
+num_episode_16 = list(episode_reward_16['num_episode'])
+episode_reward_16 = list(episode_reward_16['episodic_reward'])
+num_episode_14 = list(episode_reward_14['num_episode'])
+episode_reward_14 = list(episode_reward_14['episodic_reward'])
+
+
+num_episode_16 = num_episode_16[0]
+episode_reward_16 = episode_reward_16[0]
+num_episode_14 = num_episode_14[0]
+episode_reward_14 = episode_reward_14[0]
+
+
+# fig = plt.figure()
+plt.plot(num_episode_16, episode_reward_16, c='#236AB9', label='Distance Threshold = 300 ')
+plt.plot(num_episode_14, episode_reward_14, c='#FC7307', label='Distance Threshold = 1000')
+
+plt.legend(loc="lower right")
+plt.xlabel("Episode", fontsize = 14, family = 'Times New Roman')
+plt.ylabel("Episodic reward", fontsize = 14, family = 'Times New Roman')
+plt.title("Episode vs episodic reward for 5 UAVs : Level 3")
+plt.show()
