a8baf2b0402d62c5f6ce4f3e981dc8aac0d3ecf3
Saugat Tripathi <saugat.tripathi76@gmail.com>
2023-03-21 12:32:16 -0400

    Change in the viz code for smoothed output

--- resultplot.py
+++ resultplot.py
@@ -2,12 +2,17 @@ from scipy.io import savemat, loadmat
 import os
 import matplotlib.pyplot as plt
 import matplotlib
+import numpy as np
 
 font = {'family': 'Times New Roman',
         'size' : 12}
 
 matplotlib.rc('font', **font)
 
+def smooth(y, pts):
+    box = np.ones(pts)/pts
+    y_smooth = np.convolve(y, box, mode='same')
+    return y_smooth
 
 ###############################################################
 ##  Differernt level of information sharing for NUM_UAV = 5  ##
@@ -23,10 +28,10 @@ matplotlib.rc('font', **font)
 # Aplha / Learning Rate = 3.5e-4
 
 ## Plot for data from NUM_UAV - 5
-episode_reward_level_1 = loadmat(r'C:\Users\tripats\Documents\GitHub\Results_DQN_Pytorch\Dynamic_Environment\Run102_Dynamic\Level 1- Implicit Info Exchange\connected_user.mat')
-episode_reward_level_2 = loadmat(r'C:\Users\tripats\Documents\GitHub\Results_DQN_Pytorch\Dynamic_Environment\Run102_Dynamic\Level 2 - Reward Info Exchange\connected_user.mat')
-episode_reward_level_3 = loadmat(r'C:\Users\tripats\Documents\GitHub\Results_DQN_Pytorch\Dynamic_Environment\Run102_Dynamic\Level 3 - Position Info Exchnage (Distance Penalty)\connected_user.mat')
-# episode_reward_level_4 = loadmat(r'Results\Result_11_08\5_UAV\Level_4_Drone_State_Space_Exchange\Run_002\episodic_reward.mat')
+episode_reward_level_1 = loadmat(r'C:\Users\tripats\Documents\GitHub\Results_DQN_Pytorch\Dynamic_Environment\Run201_Dynamic\Implicit Info Exchange - Level 1\connected_user.mat')
+episode_reward_level_3 = loadmat(r'C:\Users\tripats\Documents\GitHub\Results_DQN_Pytorch\Dynamic_Environment\Run201_Dynamic\Position Info Exchnage - Level 3\Episode-450-ts-30\connected_user.mat')
+episode_reward_level_2 = loadmat(r'C:\Users\tripats\Documents\GitHub\Results_DQN_Pytorch\Dynamic_Environment\Run201_Dynamic\Reward Info Exchange - Level 2\connected_user.mat')
+episode_reward_level_4 = loadmat(r'C:\Users\tripats\Documents\GitHub\Results_DQN_Pytorch\Dynamic_Environment\Run201_Dynamic\State Info Exchange - Level 4\episodic_reward.mat')
 
 
 num_episode_1 = list(episode_reward_level_1['num_episode'])
@@ -35,29 +40,29 @@ num_episode_2 = list(episode_reward_level_2['num_episode'])
 episode_reward_2 = list(episode_reward_level_2['connected_user'])
 num_episode_3 = list(episode_reward_level_3['num_episode'])
 episode_reward_3 = list(episode_reward_level_3['connected_user'])
-# num_episode_4 = list(episode_reward_level_4['num_episode'])
-# episode_reward_4 = list(episode_reward_level_4['episodic_reward'])
+num_episode_4 = list(episode_reward_level_4['num_episode'])
+episode_reward_4 = list(episode_reward_level_4['episodic_reward'])
 
-
+pts = 1
 num_episode_1 = num_episode_1[0]
-episode_reward_1 = episode_reward_1[0]
+episode_reward_1 = smooth(episode_reward_1[0],pts) 
 num_episode_2 = num_episode_2[0]
-episode_reward_2 = episode_reward_2[0]
+episode_reward_2 = smooth(episode_reward_2[0], pts)
 num_episode_3 = num_episode_3[0]
-episode_reward_3 = episode_reward_3[0]
-# num_episode_4 = num_episode_4[0]
-# episode_reward_4 = episode_reward_4[0]
+episode_reward_3 = smooth(episode_reward_3[0], pts)
+num_episode_4 = num_episode_4[0]
+episode_reward_4 = smooth(episode_reward_4[0], pts)
 
 
 fig = plt.figure()
-plt.plot(num_episode_1, episode_reward_1, 'r', label='Level 1: Implicit')
-plt.plot(num_episode_2, episode_reward_2, 'g', label='Level 2: Reward exchange')
-plt.plot(num_episode_3, episode_reward_3, 'b', label='Level 3: Distance penalty')
-# plt.plot(num_episode_4, episode_reward_4, 'm', label='Level 4: State')
+plt.plot(num_episode_1[0:430], episode_reward_1[0:430], label='Level 1: Implicit')
+plt.plot(num_episode_2[0:430], episode_reward_2[0:430], label='Level 2: Reward exchange')
+plt.plot(num_episode_3[0:430], episode_reward_3[0:430], label='Level 3: Distance penalty')
+plt.plot(num_episode_4[0:430], episode_reward_4[0:430], label='Level 4: State')
 plt.legend(loc="lower right")
 plt.xlabel("Episode", fontsize = 14, family = 'Times New Roman')
 plt.ylabel("Connected Users", fontsize = 14, family = 'Times New Roman')
-plt.title("Connected Users vs Episode with Dynamic Users")
+plt.title("Connected Users vs Episode with Dynamic Environment")
 plt.show()
 
 # ## Plot for data from NUM_UAV - 7 and NUM_UAV - 5 Compari
