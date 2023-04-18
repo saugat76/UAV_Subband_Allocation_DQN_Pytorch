606d73277e0e212407c1de86b43e91d25c468f17
Saugat Tripathi <saugat.tripathi76@gmail.com>
2023-04-18 18:35:06 -0400

    dynamic env code reiterated with tensorboard and wandb

--- dynamic_env/main.py
+++ dynamic_env/main.py
@@ -62,7 +62,7 @@ def parse_args():
 
 
 # GPU configuration use for faster processing
-device = torch.device("cuda:0" if torch.cuda.is_available())
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 # DNN modeling
