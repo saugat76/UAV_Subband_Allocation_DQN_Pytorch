20d276348ff3b7d4ce6c41aa0ade5d0f3a9ae6fc
Saugat Tripathi <saugat.tripathi76@gmail.com>
2023-05-03 16:37:15 -0400

    addition of results

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ for i in range(1,5):
     g = subprocess.run(["python", "main.py", "--info-exchange-lvl", str(i), "--num-episode", str(1000), "--wandb-track", "True", "--learning-rate", str(2.5e-4)])
 
 #   python uav_env.py
-#   python main.py --num-episode 1000 --wandb-track True --learning-rate 2.5e-4 --info-exchange-lvl 1 --num-uav 7
+#   python main.py --wandb-track True --learning-rate 2.5e-4 --num-episode 1000 --info-exchange-lvl 4 --num-uav 7
 
 # # ## Running for level 3 with different distance values
 for i in range(0,1000, 250):
